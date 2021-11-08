59235ca3d6b91f4268a2cc634ad15ab010b53f66
richardsforu <praveen.somireddy@gmail.com>
2021-11-08 10:25:14 +0530

    ppts

--- Java SE/opps-v1/src/com/Demo.java
+++ Java SE/opps-v1/src/com/Demo.java
@@ -1,15 +1,46 @@
 package com;
 
+class Test{
+	void f3() {
+		f2();
+	}
+	public void f2() {
+		System.out.println(">> f2 method of Test class");
+	}
+}
 
 public class Demo {
 	
 	
-	void f1() { // method
+	void f1(int x) { // method
 		
 	}
+	
+	void f1(long l) {
+		
+	}
+	
+	void f1(float x) {}
+	void f1(double x) {}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		Demo d=new Demo();
+		//d.f1(1000.75);
+		
+		// How many butes? 4 ? -> 32
+		// How many bits long ->  64
+		
+		int i=2147483647;
+		long x=2147483648L;
+		//9223372036854775807
+		
+		//System.out.println(i);
+		//System.out.println(x);
+		int y=07754555;
+		System.out.println(y);
+		
+		
+		
 		
 	}
 
